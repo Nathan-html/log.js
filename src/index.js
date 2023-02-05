c4aba0d010bc8de66c8c422f03a964a241372e3d
Nathan Flacher <nathan.flacher.pro@gmail.com>
2023-02-05 23:05:29 +0100

    change index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-require('./functions/sayHello');
+const {sayHello} = require('./functions/sayHello');
+
+/**
+ * Say hello with style ;)
+ * @author Nathan Flacher <contact@nathan-flacher.com>
+ */
+module.exports.sayHello = sayHello;
